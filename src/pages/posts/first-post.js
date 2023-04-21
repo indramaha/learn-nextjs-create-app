144fbd796c1c2e72131d014658074f8064ef5541
Tugus <idabgusindra01@gmail.com>
2023-04-21 08:17:11 +0800

    learn use Image Component

--- src/pages/posts/first-post.js
+++ src/pages/posts/first-post.js
@@ -1,6 +1,23 @@
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
+
 const FirstPost = () => {
-    return (  
-        <h1>First Post</h1>
+    return ( 
+        <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                } 
+            />
+            <h1>First Post</h1>
+            <Link href="/">back to Home</Link>
+        </> 
     );
 }
  
